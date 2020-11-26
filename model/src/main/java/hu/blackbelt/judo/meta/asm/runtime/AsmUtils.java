e63f2793b25552d8e16146606ad4222b2874475d
Jozsef Borcsok <jozsef.borcsok@blackbelt.hu>
2020-11-26 14:02:55 +0100

    JNG-2008: remove MAP_PRINCIPAL operation behaviour

--- model/src/main/java/hu/blackbelt/judo/meta/asm/runtime/AsmUtils.java
+++ model/src/main/java/hu/blackbelt/judo/meta/asm/runtime/AsmUtils.java
@@ -865,10 +865,10 @@ public class AsmUtils {
         }
 
         if (getBehaviour(operation)
-                .filter(b -> OperationBehaviour.MAP_PRINCIPAL.equals(b) || OperationBehaviour.GET_PRINCIPAL.equals(b) && !EcoreUtil.equals(transferObjectType, operation.getEContainingClass()))
+                .filter(b -> OperationBehaviour.GET_PRINCIPAL.equals(b) && !EcoreUtil.equals(transferObjectType, operation.getEContainingClass()))
                 .isPresent()) {
             if (log.isDebugEnabled()) {
-                log.debug(pad(level, "    - GET_PRINCIPAL operation is exposed by it container transfer object type only, MAP_PRINCIPAL operation is not exposed"));
+                log.debug(pad(level, "    - GET_PRINCIPAL operation is exposed by it container transfer object type only"));
             }
             return;
         }
@@ -1201,8 +1201,7 @@ public class AsmUtils {
                 case VALIDATE_UPDATE:
                 case DELETE_INSTANCE:
                 case GET_TEMPLATE:
-                case GET_PRINCIPAL:
-                case MAP_PRINCIPAL: {
+                case GET_PRINCIPAL: {
                     return resolve(ownerString);
                 }
                 case LIST:
@@ -1378,12 +1377,7 @@ public class AsmUtils {
         /**
          * Get principal of a given access point.
          */
-        GET_PRINCIPAL("getPrincipal"),
-
-        /**
-         * Map principal of a given access token to access point.
-         */
-        MAP_PRINCIPAL("mapPrincipal");
+        GET_PRINCIPAL("getPrincipal");
 
         private final String type;
 
